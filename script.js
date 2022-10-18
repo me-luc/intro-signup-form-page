ee32a8da960ffec0a6557ed49c48b73b96565da4
me-luc <lukasalmeida372@gmail.com>
2022-10-18 17:55:18 -0300

    changes

--- script.js
+++ script.js
@@ -7,3 +7,36 @@ function highlight(button) {
 
 	button.classList.add("highlight");
 }
+
+function claim() {
+	const firstName = document.querySelector(".first-name").value;
+	const lastName = document.querySelector(".last-name");
+	const email = document.querySelector(".email");
+	const password = document.querySelector(".password");
+
+	//check if first name is empty
+	if (firstName === "") {
+		firstName.id = "error";
+		alert("hi");
+	} /*
+	//check if last name is empty
+	else if () {
+
+	}
+	//check if email name is empty or is an actual email
+	else if () {
+
+	}
+	//check if password name is empty  
+	else if () {
+
+	} 
+	//or is less than 8 characters
+	else if ( ) {
+
+	}
+	//if it's all ok then claim
+	else {
+
+	}*/
+}

82b262955120a10c9684a6ece687f2e822ac2f35
me-luc <lukasalmeida372@gmail.com>
2022-10-22 01:14:02 -0300

    ajustes responsividade

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ function highlight(button) {
 	}
 
 	button.classList.add("highlight");
+
+	const sibling = button.nextSibling;
+	alert(sibling);
+	if (button.innerHTML.length > 0 && !sibling.classList.contains("hide")) {
+		alert("hi");
+	}
 }
 
 function claim() {
@@ -24,6 +30,7 @@ function claim() {
 		const parent = firstName.parentNode;
 		parent.children[1].classList.remove("hide");
 		parent.children[2].classList.remove("hide");
+		parent.children[2].innerHTML = "Name shouldn't be empty";
 	}
 	//check if last name is empty
 	if (lastName.value === "") {
@@ -33,6 +40,7 @@ function claim() {
 		const parent = lastName.parentNode;
 		parent.children[1].classList.remove("hide");
 		parent.children[2].classList.remove("hide");
+		parent.children[2].innerHTML = "Last name shouldn't be empty";
 	}
 	//check if email name is empty or is an actual email
 	if (email.value === "") {
@@ -42,6 +50,17 @@ function claim() {
 		const parent = email.parentNode;
 		parent.children[1].classList.remove("hide");
 		parent.children[2].classList.remove("hide");
+		parent.children[2].innerHTML = "E-mail shouldn't be empty";
+	}
+	//check if email name is empty or is an actual email
+	if (email.value.includes("@")) {
+		error = true;
+		email.id = "error";
+		email.placeholder = "";
+		const parent = email.parentNode;
+		parent.children[1].classList.remove("hide");
+		parent.children[2].classList.remove("hide");
+		parent.children[2].innerHTML = "Doesn't look like an e-mail";
 	}
 	//check if password name is empty
 	if (password.value === "") {
@@ -51,9 +70,18 @@ function claim() {
 		const parent = password.parentNode;
 		parent.children[1].classList.remove("hide");
 		parent.children[2].classList.remove("hide");
+		parent.children[2].innerHTML = "Password shouldn't be empty";
 	}
 	//or is less than 8 characters
 	else if (password.value.length < 8) {
+		error = true;
+		password.id = "error";
+		password.placeholder = "";
+		const parent = password.parentNode;
+		parent.children[1].classList.remove("hide");
+		parent.children[2].classList.remove("hide");
+		parent.children[2].innerHTML =
+			"Password should have at least 8 characters";
 	}
 	//if it's all ok then claim
 	if (error) {
